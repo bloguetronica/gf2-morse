c6ed3f47122febe201dcf07f22877ba52aa277cb
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2024-03-22 17:57:46 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,19 +5,21 @@ apt-get -qq update
 apt-get -qq install build-essential
 apt-get -qq install libusb-1.0-0-dev
 echo Copying source code files...
-mkdir -p /usr/local/src/gf2-morse
-cp -f src/gf2-core.c /usr/local/src/gf2-morse/.
-cp -f src/gf2-core.h /usr/local/src/gf2-morse/.
-cp -f src/gf2-morse.c /usr/local/src/gf2-morse/.
+mkdir -p /usr/local/src/gf2-morse/man
+cp -f src/cp2130.cpp /usr/local/src/gf2-morse/.
+cp -f src/cp2130.h /usr/local/src/gf2-morse/.
+cp -f src/error.cpp /usr/local/src/gf2-morse/.
+cp -f src/error.h /usr/local/src/gf2-morse/.
+cp -f src/gf2device.cpp /usr/local/src/gf2-morse/.
+cp -f src/gf2device.h /usr/local/src/gf2-morse/.
+cp -f src/gf2-morse.cpp /usr/local/src/gf2-morse/.
 cp -f src/GPL.txt /usr/local/src/gf2-morse/.
 cp -f src/LGPL.txt /usr/local/src/gf2-morse/.
 cp -f src/libusb-extra.c /usr/local/src/gf2-morse/.
 cp -f src/libusb-extra.h /usr/local/src/gf2-morse/.
 cp -f src/Makefile /usr/local/src/gf2-morse/.
+cp -f src/man/gf2-morse.1 /usr/local/src/gf2-morse/man/.
 cp -f src/README.txt /usr/local/src/gf2-morse/.
-echo Building and installing binaries...
-make -C /usr/local/src/gf2-morse all install clean
-echo Installing man pages...
-mkdir -p /usr/local/share/man/man1
-cp -f man/gf2-morse.1.gz /usr/local/share/man/man1/.
+echo Building and installing binaries and man pages...
+make -C /usr/local/src/gf2-morse install clean
 echo Done!
